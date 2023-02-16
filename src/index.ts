d5ad30160b4141aee808910cbfb15d1278ccbfbf
Carles Galan <galan.carles@gmail.com>
2023-02-16 19:41:36 +0100

    Create server and enviromen variablesdownloading express and debug

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const a = "hola";
+import express from "express";
+import "./loadEnviroments.js";
+
+const port = process.env.PORT ?? 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ "hello world": "hola" });
+});
+app.listen(port);
